bbce4bd164fbf092df24a4ad14fef7f1c9f62d72
Iwancof <rock.iwancof@gmail.com>
2019-02-10 17:30:37 +0900

    プロジェクト ファイルを追加します。

---
+++ My_Neural_Network_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace My_Neural_Network_2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
