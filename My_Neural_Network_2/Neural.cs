1e45cc6df001bbb3be260f7170f3dcef4f785fed
Iwancof <rock.iwancof@gmail.com>
2019-02-10 20:37:16 +0900

    作業再開。これから重みの大きさを拡張して写経代わりに実装してみる。

--- My_Neural_Network_2/Neural.cs
+++ My_Neural_Network_2/Neural.cs
@@ -29,5 +29,44 @@ namespace My_Neural_Network_2
                     D(Output_Weigth[i, j].ToString());
                 }
         }
+
+        double[] Errors;
+        public void Train(double[][] Input, double[][] Answer,double Epsilon,int epoch) {
+            Errors = new double[epoch];
+            for(int TrainCount = 0;TrainCount < epoch; TrainCount++) {
+                for(int DataCount = 0;DataCount < Input.GetLength(0); DataCount++) {
+                    double[] inputData = Input[DataCount];
+                    double[] answerData = Answer[DataCount];
+                }
+            }
+        }
+
+        public void Update_Weigth(double[] input,double[] answer,double Epsilon) {
+            Calc(input);
+
+            double[] Output_Delta = {
+                (OutputData[0] - answer[0]) * OutputData[0] * (1d - OutputData[0]), 
+                (OutputData[1] - answer[1]) * OutputData[1] * (1d - OutputData[1])
+                };
+        }
+
+
+
+        double[] InputData;
+        double[] HiddenData;
+        double[] OutputData;
+        public double[] Calc(double[] Input) { //__forward
+            InputData = (double[])Input.Clone();
+            HiddenData = new double[]{
+                Math.In_Product(InputData,Math.Dimension_Down(Hidden_Weigth,0)),
+                Math.In_Product(InputData, Math.Dimension_Down(Hidden_Weigth, 1))
+            };
+            OutputData = new double[] {
+                Math.In_Product(HiddenData,Math.Dimension_Down(Output_Weigth,0)),
+                Math.In_Product(HiddenData, Math.Dimension_Down(Output_Weigth, 1))
+            };
+            return OutputData;
+        }
+
     }
 }
